e6055c085467dbb302223bce734b971879157e21
llovepeaches <45651278+llovepeaches@users.noreply.github.com>
2023-12-09 16:22:24 +0000

    Merge pull request #7 from llovepeaches/dependabot/npm_and_yarn/hexo-theme-anzhiyu-1.6.12
    Bump hexo-theme-anzhiyu from 1.6.9 to 1.6.12 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/11/21/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/12/09/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
